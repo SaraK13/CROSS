797e2d64464d488b25207fbc32f01f8ff100d85e
SaraK13 <if22b237@technikum-wien.at>
2024-10-04 16:25:14 +0200

    Integrated PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,48 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), 
+            VitePWA({ 
+              registerType: 'autoUpdate',
+              includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+              manifest: {
+                name: 'My CROSS project',
+                short_name: 'CROSS',
+                description: 'My CROSS Ionic project in 5th sem',
+                theme_color: '#000',
+                display: "standalone",
+                icons: [
+                  {
+                    src: './src/images/pwa-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                    purpose: 'any'
+                  },
+                  {
+                    src: './src/images/pwa-maskable-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                    purpose: 'maskable',
+                  },
+                  {
+                    src: './src/images/pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any'
+                  },
+                  {
+                    src: './src/images/pwa-maskable-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable',
+                  },
+                ],
+              },
+              devOptions: {
+                enabled: true
+                /* other options */
+              },
+              })],
   server: {
     watch: {
       usePolling: true,
